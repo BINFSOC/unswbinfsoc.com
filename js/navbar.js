86542242bb8339d15cd504a1aaa3d1d95e8b0165
Enzo <lorenzo.scarpellino@hotmail.com>
2025-10-26 17:59:10 +1100

    mobile fixes

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,21 @@
+//Navigation
 const nav = document.getElementById("navigation");
 
 //Slideout menu
-
 const slideout = document.getElementById("slideout-menu")
-setTimeout(function(){slideout.style.visibility="hidden";},1000);
+setTimeout(function(){slideout.style.visibility="hidden";}, 1000);
+
+window.addEventListener('click', function(e){
+    if (!slideout.contains(e.target) && !document.getElementById('menu-icon').contains(e.target)){
+        slideout.style.visibility="hidden";
+    }
+  });
+
+
+function slideoutMenu(){
+    if (window.getComputedStyle(slideout).getPropertyValue('visibility')=="hidden"){
+        slideout.style.visibility ="visible";
+    } else {
+        slideout.style.visibility="hidden";
+    }
+}
