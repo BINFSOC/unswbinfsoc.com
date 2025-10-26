695f52d2b4d36a0710d30e3edec0cf7cbdd1e7fa
Enzo <lorenzo.scarpellino@hotmail.com>
2025-10-26 16:59:59 +1100

    adding fix for navbar on mobile

---
+++ js/navbar.js
@@ -0,0 +1,6 @@
+const nav = document.getElementById("navigation");
+
+//Slideout menu
+
+const slideout = document.getElementById("slideout-menu")
+setTimeout(function(){slideout.style.visibility="hidden";},1000);
